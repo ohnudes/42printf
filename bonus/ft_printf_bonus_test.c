31f5468c5af71d74f04533bdf6d9f77fc31f792d
Nicolas Maturana Chandia <nmaturan@car6s5.42barcelona.com>
2023-08-14 21:52:50 +0200

    Update: printf splitting mixed in results because of its buffer feature.

--- bonus/ft_printf_bonus_test.c
+++ bonus/ft_printf_bonus_test.c
@@ -6,54 +6,83 @@
 /*   By: nmaturan <nmaturan@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 19:01:50 by nmaturan          #+#    #+#             */
-/*   Updated: 2023/08/14 10:04:15 by ohadmin          ###   ########.fr       */
+/*   Updated: 2023/08/14 20:50:59 by nmaturan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include <limits.h>
 #include <stdio.h>
 
-void	main_spec(int result);
-//void	sum_test(int result);
+void	main_spec(void);
+void	sum_test(void);
 
 int	main(void)
 {
-	int	result;
-
-	result = 0;
-
 	// tests
-	main_spec(result);
-	//sum_test(result);
+	//main_spec();
+	//sum_test();
+	int result = printf("%+d", -1);
+	printf("\nresult = %d\n", result);
 	return (0);
 }
+//sum_test(result);
 
 // comparing my printf results with original printf
-void	main_spec(int result)
+void	sum_test(void)
 {
+	int	result;
+	int	*a;
+
+	result = 0;
+	a = NULL;
 	printf("\n>>SPEC_TEST<<\n\n");
+
+	ft_printf("\n>> %%d <<\n");
+
+	result = ft_printf("% d", 42);
+	ft_printf("\nft_printf result = %d\n", result);
+	result = printf("% d", 42);
+	printf("\nprintf result = %d\n", result);
+	
+	ft_printf("\n>> %%i <<\n");
+
+	result = ft_printf("% i", -42);
+	ft_printf("\nft_printf result = %d\n", result);
+	result = printf("% i", -42);
+	printf("\nprintf result = %d\n", result);
+}
+
+void	main_spec(void)
+{
+	int	result;
+	int	*a;
+
+	result = 0;
+	a = NULL;
+	printf("\n>>SPEC_TEST<<\n\n");
+
+	ft_printf("\n>> %%d <<\n");
 
 	result = ft_printf("%d", 42);
 	ft_printf("\nft_printf result = %d\n", result);
 	result = printf("%d", 42);
 	printf("\nprintf result = %d\n", result);
 	
-	ft_printf("\n\n");
+	ft_printf("\n>> %%i <<\n");
+
 	result = ft_printf("%i", -42);
 	ft_printf("\nft_printf result = %d\n", result);
 	result = printf("%i", -42);
 	printf("\nprintf result = %d\n", result);
 
-	ft_printf("\n\n");
+	ft_printf("\n>> %%p <<\n");
 
-	int	a;
-	result = ft_printf("%p", &a);
-	ft_printf("\nft_printf result = %d\n", result);
-	result = printf("%p", &a);
-	ft_printf("\nprintf result = %d\n", result);
+	result = ft_printf("%p", a);
+	ft_printf(" ft_printf result = %d\n", result);
+	result = printf("%p", a);
+	ft_printf(" printf result = %d\n", result);
 
-	ft_printf("\n\n");
+	ft_printf("\n>> %%c <<\n");
 
 	result = ft_printf("%c", '@');
 	ft_printf("\nft_printf result = %d\n", result);
@@ -62,7 +91,6 @@ void	main_spec(int result)
 	
 	ft_printf("\n\n");
 
-	/*
 	result = ft_printf("%s", "Ayyy lmaooOOo");
 	ft_printf("\nft_printf result = %d\n", result);
 	result = printf("%s", "Ayyy lmaooOOo");
@@ -70,9 +98,9 @@ void	main_spec(int result)
 
 	ft_printf("\n\n");
 
-	result = ft_printf("%u", INT_MAX);
+	result = ft_printf("%u", 2147483647);
 	ft_printf("\nft_printf result = %d\n", result);
-	result = printf("%u", INT_MAX);
+	result = printf("%u", 2147483647);
 	ft_printf("\nprintf result = %d\n", result);
 
 	ft_printf("\n\n");
@@ -89,5 +117,4 @@ void	main_spec(int result)
 	result = printf("%X", 123456);
 	ft_printf("\nprintf result = %d\n", result);
 	ft_printf("\n\n");
-	*/
 }
