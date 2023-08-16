40fd8b954e7bd2961d4c059581fa97f174b16116
Nudes <nivohq@gmail.com>
2023-08-16 22:14:59 +0200

    Update: New flag "FLAG" for t_argformat. Plus cleaning on unnecessary functions and updating changing types in strchr.

--- bonus/ft_printf.h
+++ bonus/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: nmaturan <nmaturan@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/30 16:14:22 by nmaturan          #+#    #+#             */
-/*   Updated: 2023/08/14 21:36:26 by nmaturan         ###   ########.fr       */
+/*   Updated: 2023/08/16 21:01:01 by nmaturan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,35 +19,34 @@
 
 typedef struct	s_argformat
 {
-	// ft_printf return
-	int	count;
+	char	*spec;	// *spec == spec ocurrence
+	int		count;	// ft_printf return
 
 	// IF FLAGS DETECTED
-	int	precount;
-	int	s_len;
+	int		flags;	// flag used in printc to count for total len.
+	int		s_len;
 
 	// flags indicator
-	int	sum;
-	int	space;
-	int	dash;
-	int	zero;
-	int	hash;
-	int	width;
-	int	precision;
-}		t_argformat;
+	int		sum;
+	int		space;
+	int		dash;
+	int		zero;
+	int		hash;
+	int		width;
+	int		precision;
+}			t_argformat;
 
 int		ft_printf(const char *str, ...);
 
 // checking for flags
 void	flag_parser(t_argformat *total, va_list args, char *start, char *set);
-char	*check_valid_format(const char *str);
-void	flag_sum_space(const char *str, t_argformat *total);
+char	*check_valid_format(const char *str, int *flags);
 
 // no flags
 int		format_handler(t_argformat *total, va_list args, const char format);
 
 // utils
-char	*ft_strchr(const char *str, int set);
+char	*ft_strchr(const char *str, char set);
 void	ft_printc(t_argformat *total, char n);
 void	ft_prints(t_argformat *total, char *s);
 
